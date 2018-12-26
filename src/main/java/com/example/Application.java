5a9ac2ce1503b4160ea451a475b7f6a521bf3f37
Bjorne1 <35253647+Bjorne1@users.noreply.github.com>
2018-12-26 11:12:13 +0800

    add author info

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,11 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wcs
+ */
 @SpringBootApplication
 @MapperScan("com.example.dao")
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
